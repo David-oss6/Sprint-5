47400aaa506b78409781106976cb94528f45420b
David <im.david85@gmail.com>
2021-09-09 18:09:06 +0200

    ok

--- sprint.ts
+++ sprint.ts
@@ -1,10 +1,21 @@
 // - Exercici 1 i 2
-var reportAcudits: object = [];
+var reportAcudits = [];
 var position: number = -1;
 var correcto: boolean = false;
+var date = new Date();
+date.toISOString();
 
-if (correcto == true) {
-    document.querySelector('#nextJoke').addEventListener('click', () => {
+// interface IreportAcudits {
+//     joke: string;
+//     score: number;
+//     date: Date;
+// }
+
+document.querySelector('#nextJoke').addEventListener('click', () => {
+    if (correcto == false && position >= 0) {
+        document.querySelector('#peticion').innerHTML = 'PLease rate the Joke'
+    } else {
+        correcto = false;
         fetch('https://icanhazdadjoke.com/', {
             headers: {
                 'Accept': 'application/json',
@@ -15,37 +26,45 @@ if (correcto == true) {
             })
             .then(function (res) {
                 document.querySelector('#printJoke').innerHTML = `${res.joke}`;
-                reportAcudits = [
+                reportAcudits.push(
                     {
-                        joke: res.joke,
-                        score: 0,
-                        date: Date,
+                        "joke": res.joke,
+                        "score": 0,
+                        "date": date,
                     }
-                ]
+                );
+
                 position += 1;
-                correcto = false;
+                console.log(reportAcudits)
+                console.log(position)
             });
+
+    }
+
+    document.querySelector('.score1').addEventListener('click', () => {
+        reportAcudits[position].score = 1;
+        correcto = true;
+        console.log(reportAcudits[position])
     })
-} else {
-    document.querySelector('#peticion').innerHTML = 'Please rate the Joke'
-}
 
-document.querySelector('.score1').addEventListener('click', () => {
-    reportAcudits[position].score = 1;
-    correcto = true;
+    document.querySelector('.score2').addEventListener('click', () => {
+        reportAcudits[position].score = 2;
+        correcto = true;
+        console.log(reportAcudits[position])
+    })
+
+    document.querySelector('.score3').addEventListener('click', () => {
+        reportAcudits[position].score = 3;
+        correcto = true;
+        console.log(reportAcudits[position])
+    })
+
+
 })
 
-document.querySelector('.score2').addEventListener('click', () => {
-    var score: number = 2;
-    reportAcudits[position].score = 2;
-    correcto = true;
-})
 
-document.querySelector('.score3').addEventListener('click', () => {
-    var score: number = 3;
-    reportAcudits[position].score = 3;
-    correcto = true;
-})
+console.log(reportAcudits);
+
 
 
 
