422be3c919cd411411740964b997abb6b0e0f4ed
David <im.david85@gmail.com>
2021-09-10 16:01:25 +0200

    Empezando ej 6

--- sprint.js
+++ sprint.js
@@ -85,7 +85,7 @@ document.querySelector('.score3').addEventListener('click', function () {
     }
 });
 // console.log(reportAcudits);
-//      Nivell 2 Exercici 4 *************************
+//      Nivell 2 Exercici 4 i 5  *************************
 fetch('https://api.openweathermap.org/data/2.5/onecall?lat=41.38&lon=2.16&exclude=hourly,daily&appid=972056032b2d7a22b30500165a06d9d9')
     .then(function (res) {
     return res.json();
@@ -94,3 +94,4 @@ fetch('https://api.openweathermap.org/data/2.5/onecall?lat=41.38&lon=2.16&exclud
     document.querySelector('#forecast').innerHTML = "" + res.alerts[0].description;
     console.log(res);
 });
+// Nivell 3 Exercici 6 **************************
